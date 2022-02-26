edb7abb426119f7654c436b0dec531840d713e3e
Merve Sahin <merve.nur@outlook.com>
2022-02-26 15:38:29 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time<11)
+               Console.WriteLine("Gunaydin!");
+            else if (time <= 18)
+               Console.WriteLine("iyi gunler!");
+            else 
+               Console.WriteLine("iyi geceler!");
+            
+            string sonuc = time <= 18 ? "iyi gunler!": "iyi geceler!";
+            //Console.WriteLine(sonuc);
+
+            sonuc = time >= 6 && time<11 ?  "Gunaydin!" : time<= 18 ? "iyi gunler!": "iyi geceler!";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
